5ade07502edd9d1ea431943d49a19b791c639bcd
eastolfi <astolfi_wotc@hotmail.com>
2017-09-29 15:15:42 +0200

    Dynamic and commented

--- setup.sh
+++ setup.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
-git config --local --rename-section alias 'alias-old'
 
-git config --local alias.lol 'log --oneline --decorate --graph --abbrev-commit --all'
-git config --local alias.st 'status'
-git config --local alias.lines 'log --oneline --decorate'
-git config --local alias.undo-soft 'reset --soft HEAD^'
-git config --local alias.undo-hard 'reset --hard HEAD^'
-git config --local alias.s 'status -s'
-git config --local alias.pullr 'pull --rebase'
-git config --local alias.pending-branches 'branch --no-merged'
-git config --local alias.merged-branches 'branch --merged'
-git config --local alias.f 'fetch'
+c_type="local"
 
-echo "The aliases has been included. All the old aliases are new within 'alias-old'"
+case $1 in
+	global)
+		c_type="--global"
+	;;
+	system)
+		c_type="--system"
+	;;
+	*)
+		c_type="--local"
+	;;
+esac
+
+# Rename old aliases
+eval "git config $c_type --rename-section alias 'alias-old'"
+
+# Basics
+eval "git config $c_type alias.st 'status'"
+eval "git config $c_type alias.s 'status -s'"
+eval "git config $c_type alias.f 'fetch'"
+eval "git config $c_type alias.p 'pull'"
+
+# Logging
+eval "git config $c_type alias.lol 'log --oneline --decorate --graph --abbrev-commit --all'"
+eval "git config $c_type alias.lines 'log --oneline --decorate'"
+eval "git config $c_type alias.logfile 'log --follow'"
+
+# Alterations
+eval "git config $c_type alias.undo.soft 'reset --soft HEAD^'"
+eval "git config $c_type alias.undo.hard 'reset --hard HEAD^'"
+
+# Branches
+eval "git config $c_type alias.branches.unmerged 'branch --no-merged'"
+eval "git config $c_type alias.branches.merged 'branch --merged'"
+
+# Utils
+eval "git config $c_type alias.pullr 'pull --rebase'"
+eval "git config $c_type alias.visual '!gitk'"
+
+
+echo "The aliases has been included in '$c_type' config. All the old aliases are new within 'alias-old'"
