4116aae04d375863c51facc90fb657a47e6049eb
eastolfi <astolfi_wotc@hotmail.com>
2020-07-21 14:23:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,6 +24,7 @@ eval "git config $c_type alias.st 'status'"
 eval "git config $c_type alias.s 'status -s'"
 eval "git config $c_type alias.f 'fetch'"
 eval "git config $c_type alias.p 'pull'"
+eval "git config $c_type alias.cp 'cherry-pick'"
 
 # Logging
 eval "git config $c_type alias.lol 'log --oneline --decorate --graph --abbrev-commit --all'"
@@ -33,6 +34,7 @@ eval "git config $c_type alias.logfile 'log --follow'"
 # Alterations
 eval "git config $c_type alias.undo.soft 'reset --soft HEAD^'"
 eval "git config $c_type alias.undo.hard 'reset --hard HEAD^'"
+eval "git config $c_type alias.undo.first 'update-ref -d HEAD'"
 
 # Branches
 eval "git config $c_type alias.branches.unmerged 'branch --no-merged'"
@@ -43,6 +45,11 @@ eval "git config $c_type alias.branches.prune 'remote prune origin'"
 eval "git config $c_type alias.pullr 'pull --rebase'"
 eval "git config $c_type alias.visual '!gitk'"
 eval "git config $c_type alias.fst '!git fetch && git status'"
+eval "git config $c_type alias.conf 'git config -l"
+
+# Initialization
+# Use it as git init.user "Username" "email"
+eval "git config $c_type alias.init.user '!f() { git config --local user.name \$1 && git config --local user.email \"\$2\"; }; f'"
 
 # patching
 v_patch_stash="git config $c_type alias.patch.stash '!f() { git show -p stash@{\"\$1\"} > \"\$2.patch\"; }; f'"
